5f5b3c3df3ff6bcf9d33e732413339adaaff51a6
victor <victorgomesdacosta18@gmail.com>
2023-10-11 17:12:25 -0300

    Initial

---
+++ app/components/header/index.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return {
+        < div className = "container" >
+            <div className="logotipo"></div>
+            <div className="menu"></div>
+            <div className="action"></div>
+        </div >
+    };
+};
